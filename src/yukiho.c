164a6f3202f336201b49a00f9dbfb1f46abdc5e3
sgrsmt <sgrsmt@outlook.com>
2012-10-15 10:33:46 +0900

    initial works for use of Lua C API

--- src/yukiho.c
+++ src/yukiho.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
+#include <lua.h>
+#include <luaconf.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+
+static int pmain(lua_State* L) {
+  fprintf(stderr, "Hello, Lua C API! "); 
+  return 1; 
+}
+
+static const luaL_Reg exports[] = {
+  { "pmain", pmain }, 
+  { NULL, NULL }
+}; 
 
 int main(int argc, char const* argv[])
 {
-  puts("Hello, World"); 
-  return 0;
+  lua_State* L = luaL_newstate(); 
+  if (L == NULL) {
+    fprintf(stderr, "cannot create state: not enough memory"); 
+    //return EXIT_FAILURE; 
+    return -1; 
+  }
+  luaL_openlibs(L); 
+  //luaL_register(L, NULL, exports); 
+  lua_pushcfunction(L, pmain); 
+  int status = lua_pcall(L, 0, 0, 0); 
+  lua_close(L); 
+  return status;
 }
 
